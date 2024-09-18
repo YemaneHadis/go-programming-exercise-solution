7d7617b2cfcfef638c83ddc1c41ec62fc84f6b6a
yemane <yemane.hadis@gmail.com>
2024-09-18 18:36:47 -0400

    Solving Ex4.1

--- chapter4/exercise4.1.go
+++ chapter4/exercise4.1.go
@@ -1,13 +1,39 @@
+/***
+	- Write a function that counts the number of bits that are different in two
+	- SHA256 hashes (NB it is difference in bit not byte)
+***/
+
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
 )
 
+func DifferentBits(h1, h2 *[sha256.Size]byte) int {
+	// h1 and h2 are the sha256 representation of the two string
+	count := 0
+	for i := range h1 {
+		for b := h1[i] ^ h2[i]; b != 0; b &= b - 1 {
+			count++
+		}
+		/**
+		 h2[i] ^ h2[i] - this will give us number of diffrent bit between thise two byte
+		 if they are the same char this will be zero
+		 else we will have one for different bits and 0 for same bits and the value is stored in b
+		 b &= b-1 will remove 1 one bit from the byte  and we are incrmenting our  count
+		**/
+	}
+	return count
+}
+
 func main() {
-	fmt.Printf("s1: %s h1: %X h1 type: %T\n", s1, h1, h1)
-	fmt.Printf("s2: %s h2: %X h2 type: %T\n", s2, h2, h2)
-	fmt.Printf("Number of different bits: %d\n", DifferentBits(h1, h2))
+	s1 := "helloworld"
+	s2 := "HELLOWORLD"
+	h1 := sha256.Sum256([]byte(s1))
+	h2 := sha256.Sum256([]byte(s2))
+	fmt.Printf("%x\n", h1)
+	fmt.Printf("Number of different bits between sha256 representation of %s %s are %d\n", s1, s2, DifferentBits(&h1, &h2))
 }
 
 // count the number of bits set in x
