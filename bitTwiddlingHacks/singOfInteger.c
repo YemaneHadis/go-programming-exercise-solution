7916054dc9feb01ce8e835dbc6fe80a24bb36079
yemane <yemane.hadis@gmail.com>
2024-09-18 18:00:55 -0400

    Counting Set bit

--- bitTwiddlingHacks/singOfInteger.c
+++ bitTwiddlingHacks/singOfInteger.c
@@ -2,6 +2,57 @@
 #include<stdbool.h>
 
 
+// Naive way 
+int countSetBit(int u){
+ int count = 0;
+ int number = u;
+ for (; u > 0; u >>=1)
+ {
+   count += u&1;
+ }
+ printf("Number Of Set bits in \t%d  are\t %d  \n",number, count);
+ return 0;
+}
+
+
+
+//Kernighan way of doing it 
+// this appraoch would eliminate unecessary steps 
+// for power two and so 
+int countSetBits2(int u){
+    int number = u;
+    int count = 0;
+    for(;u >0; u&=u-1){
+        count++;
+    }
+    printf("Using Kernighan Method Number Of Set bits in \t%d  are\t %d  \n",number, count);
+
+    return 0;
+}
+
+//Sign extending from a variable bit-width
+int singExtending(){
+    unsigned int b;
+    int x;
+    int y;
+    int const m = 1U <<(b-1) ;//mask can be precomputed if b is fixed
+    //x = x &   
+    return 0;
+}
+
+
+int isPowOfTwo(unsigned int u){
+    
+    bool isPowerOfTwo = (u&(u-1)) == 0;
+    // in the above 0 is classified as power of two wrongly 
+   
+    bool isPowerOfTwo2 =  u && !(u & (u - 1));
+
+    printf("%d is Power of Two Evaluated to %s\n", u,isPowerOfTwo?"True":"False");
+    return 0;
+
+
+}
 int abs(int u){
     /**
      *  If u is +ve number then mask will be 0 u xor mask will be resulted to u
@@ -9,8 +60,9 @@ int abs(int u){
      * 
      */
     unsigned int r;
-    int const mask = u >> sizeof(int) * __CHAR_BIT__-1;
+    int const mask = u >> (sizeof(int) * __CHAR_BIT__-1);
     r = (u-mask) ^ mask;
+    return 0;
 }
 int sing (int u, int v){
     int sing1;
@@ -21,6 +73,7 @@ int sing (int u, int v){
 
     printf("Sing of U\t%d\n", sing1);
     printf("Sing of V\t%d\n", sing2);
+    printf("Unsinged Int Size of V\t%lu\n", sizeof(unsigned int));
 
     /* 
     - HOW WE ARE GETTING THE SING OF AN INTEGER 
@@ -54,5 +107,17 @@ int main(int argc, char const *argv[])
     sing(12,-11);
     checkSameSign(12,11);
     checkSameSign(12,-12);
+
+    countSetBit(11);
+    countSetBit(16);
+
+    /**
+     *  this approch is more efficient from number of 
+     *  operation perspective  but we are going to do 32 operation in the 
+     *  worest case if the number is int and int is 4 byte 
+     * 
+     * **/
+    countSetBits2(11);
+    countSetBits2(16);
     return 0;
 }
